3daed96c208a895ccb27cf46f8f03f2727738f57
ElPhysico <vkkleinqt@gmail.com>
2025-10-09 04:37:02 +0200

    added force=True such that logging setup is forced even in jupyter

--- src/mlbt/log_utils.py
+++ src/mlbt/log_utils.py
@@ -7,5 +7,6 @@ def setup_logging(verbose: bool = False) -> None:
     logging.basicConfig(
         level=level,
         format="%(asctime)s | %(levelname)s | %(message)s",
-        datefmt="%d-%m-%Y %H:%M:%S"
+        datefmt="%d-%m-%Y %H:%M:%S",
+        force=True
     )
