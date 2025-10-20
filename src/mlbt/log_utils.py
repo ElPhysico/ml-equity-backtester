f73be851772e61779dbb06050fc52746dff0d7a8
ElPhysico <vkkleinqt@gmail.com>
2025-10-20 13:01:29 +0200

    changed base logging level to warning

--- src/mlbt/log_utils.py
+++ src/mlbt/log_utils.py
@@ -3,10 +3,11 @@ import logging
 
 
 def setup_logging(verbose: bool = False) -> None:
-    level = logging.DEBUG if verbose else logging.INFO
+    level = logging.DEBUG if verbose else logging.WARNING
     logging.basicConfig(
         level=level,
         format="%(asctime)s | %(levelname)s | %(message)s",
         datefmt="%d-%m-%Y %H:%M:%S",
         force=True
     )
+    logging.getLogger('matplotlib').setLevel(logging.WARNING)
